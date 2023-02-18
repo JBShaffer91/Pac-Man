35631f936abac6eab02e45a98ad0ae8b0a9184d6
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-17 23:07:58 -0800

    create Pac-Man and add it to the canvas

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ const maze = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-// Define the tile size
+// define the tile size
 const TILE_SIZE = 20;
 
 // loop through the maze array
@@ -33,3 +33,50 @@ for (let i =0; i < maze.length; i++) {
     }
   }
 }
+
+// define the Pac-Man object with initial properties
+const pacman = {
+  x: 100, // initial x position
+  y: 100, // initial y position
+  radius: 10, // Pac-Man's radius
+  mouth: 0, // Pac-Man's mouth opening
+  speed: 5, // Pac-Man's speed
+  direction: "right" // Pac-Man's initial direction
+}
+
+// draw Pac-Man on the canvas
+function drawPacman() {
+  // set the fill style to yellow for the body
+  ctx.fillStyle = "yellow";
+
+  // create a new path for Pac-Man
+  ctx.beginPath();
+
+  //Use the arc() method to create a circle shape for Pac-Man
+  ctx.arc(pacman.x, pacman.y, pacman.radius, pacman.mouth, Math.PI * 2 - pacman.mouth);
+
+  // fill the circle with the fill style
+  ctx.fill();
+
+  // update Pac-Man's mouth opening
+  pacman.mouth += 0.1;
+
+  // if mouth is fully open or closed, reverse the direction
+  if (pacman.mouth > Math.PI / 4 || pacman.mouth <0) {
+  pacman.speed = -pacman.speed;
+  }
+
+  // move Pac-Man in the current direction
+  if (pacman.direction === "right") {
+  pacman.x += pacman.speed;
+  } else if (pacman.direction === "left") {
+  pacman.x -= pacman.speed;
+  } else if (pacman.direction === "up") {
+  pacman.y -= pacman.speed;
+  } else if (pacman.direction === "down") {
+  pacman.y += pacman.speed;
+  }
+}
+
+// draw Pac-Man to the canvas
+drawPacman();
