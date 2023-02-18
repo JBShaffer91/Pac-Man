d067767b8a4fa67ec642edc3280c7cca53a35839
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-17 22:38:15 -0800

    create the maze in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
+// get reference to canvas, get 2D drawing context
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+// create the maze
+const maze = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
