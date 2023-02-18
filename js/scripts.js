8cdc9feb08f1e1171e0ba62a7e82ab72e3bb94c8
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-17 22:46:40 -0800

    loop through maze array, draw wall and floor tiles

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,20 @@ const maze = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+// Define the tile size
+const TILE_SIZE = 20;
+
+// loop through the maze array
+for (let i =0; i < maze.length; i++) {
+  for (let j = 0; j < maze[i].length; j++) {
+    if (maze[i][j] === 1) {
+      // draw the wall tile
+      ctx fillStyle - "blue";
+      ctx.fillRect(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    } else {
+      // draw the floor tile
+      ctx fillStyle - "black";
+      ctx.fillRect(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+  }
+}
